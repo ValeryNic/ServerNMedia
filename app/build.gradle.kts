197e30ed4f9e56c62ca7b08dffce0355a8e9e39d
Valery <vn_ulanov@mail.ru>
2023-12-28 12:58:35 +0300

    MultiLoadAndroid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,11 +53,11 @@ android {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("com.google.android.gms:play-services-basement:18.2.0")
     implementation("com.google.android.gms:play-services-base:18.2.0")
-    implementation("com.google.firebase:firebase-messaging-ktx:23.3.1")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("androidx.room:room-common:2.6.1")
@@ -69,9 +69,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     val coreKtxVersion = "1.12.0"
     implementation("androidx.core:core-ktx:$coreKtxVersion")
-    implementation("androidx.activity:activity-ktx:1.8.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -79,6 +79,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     val gson_version= "2.10.1"
     implementation("com.google.code.gson:gson:$gson_version")
+    val glide_version = "4.16.0"
+    implementation("com.github.bumptech.glide:glide:$glide_version")
 
 }
 
--- app/src/main/java/ru/netology/servernmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/servernmedia/activity/AppActivity.kt
@@ -1,79 +1,154 @@
 package ru.netology.servernmedia.activity
 
+import android.graphics.BitmapFactory
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import okhttp3.*
+import ru.netology.servernmedia.databinding.ActivityAppBinding
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.messaging.FirebaseMessaging
+import okhttp3.internal.notify
 import ru.netology.servernmedia.R
 import ru.netology.servernmedia.activity.NewPostFragment.Companion.textArg
 
 class AppActivity : AppCompatActivity(R.layout.activity_app) {
+    private val urls = listOf("netology.jpg", "sber.jpg", "tcs.jpg", "404.png")
+    private var index = 0
+    private val worker = WorkerThread().apply {
+        start()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        requestNotificationsPermission()
-
-        intent?.let {
-            if (it.action != Intent.ACTION_SEND) {
-                return@let
+        val binding = ActivityAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val handler = Handler(Looper.getMainLooper())
+        worker.callback = {
+            handler.post {
+                binding.image.setImageBitmap(it)
             }
-
-            val text = it.getStringExtra(Intent.EXTRA_TEXT)
-            if (text?.isNotBlank() != true) {
-                return@let
-            }
-
-            intent.removeExtra(Intent.EXTRA_TEXT)
-            findNavController(R.id.nav_host_fragment)
-                .navigate(
-                    R.id.action_feedFragment_to_newPostFragment,
-                    Bundle().apply {
-                        textArg = text
-                    }
-                )
         }
 
-        checkGoogleApiAvailability()
-    }
-
-    private fun requestNotificationsPermission() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-            return
-        }
-
-        val permission = Manifest.permission.POST_NOTIFICATIONS
-
-        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
-            return
-        }
-
-        requestPermissions(arrayOf(permission), 1)
-    }
-
-    private fun checkGoogleApiAvailability() {
-        with(GoogleApiAvailability.getInstance()) {
-            val code = isGooglePlayServicesAvailable(this@AppActivity)
-            if (code == ConnectionResult.SUCCESS) {
-                return@with
+        binding.load.setOnClickListener {
+            if (index == urls.size) {
+                index = 0
             }
-            if (isUserResolvableError(code)) {
-                getErrorDialog(this@AppActivity, code, 9000)?.show()
-                return
-            }
-            Toast.makeText(this@AppActivity, R.string.google_play_unavailable, Toast.LENGTH_LONG)
-                .show()
-        }
 
-        FirebaseMessaging.getInstance().token.addOnSuccessListener {
-            println(it)
+            val url = "http://10.0.2.2:9999/avatars/${urls[index++]}"
+//            Glide.with(binding.image)
+//                .load(url)
+//                .placeholder(R.drawable.ic_loading_100dp)
+//                .error(R.drawable.ic_error_100dp)
+//                .timeout(10_000)
+//                .into(binding.image)
+
+//            Picasso.get()
+//                .load(url)
+//                .error(R.drawable.ic_error_100dp)
+//                .into(binding.image);
         }
     }
 }
+class WorkerThread: Thread(){
+    private lateinit var handler: Handler
+    var callback:(Bitmap) -> Unit = {}
+    private val okHttpClient=OkHttpClient.Builder()
+        .connectTimeout(30,TimeUnit.SECONDS)
+        .build()
+    fun download (url:String){
+        val message = handler.obtainMessage()
+        message.obj = url
+        handler.sendMessage(message)
+    }
+    override fun run(){
+        Looper.prepare()
+        handler=Handler(Looper.myLooper()!!){message:Message ->
+            val request = Request.Builder()
+                .url(message.obj as String)
+                .build()
+
+            val result = okHttpClient.newCall(request)
+                .execute()
+            println(result.body?.string()?.length)
+            callback(BitmapFactory.decodeStream(result.body?.byteStream()))
+
+            true
+        }
+        Looper.loop()
+    }
+
+}
+//fun myDouble(){//остатки прошлой программы
+//        requestNotificationsPermission()
+//
+//        intent?.let {
+//            if (it.action != Intent.ACTION_SEND) {
+//                return@let
+//            }
+//
+//            val text = it.getStringExtra(Intent.EXTRA_TEXT)
+//            if (text?.isNotBlank() != true) {
+//                return@let
+//            }
+//
+//            intent.removeExtra(Intent.EXTRA_TEXT)
+//            findNavController(R.id.nav_host_fragment)
+//                .navigate(
+//                    R.id.action_feedFragment_to_newPostFragment,
+//                    Bundle().apply {
+//                        textArg = text
+//                    }
+//                )
+//        }
+//
+//        checkGoogleApiAvailability()
+//    }
+//
+//    private fun requestNotificationsPermission() {
+//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+//            return
+//        }
+//
+//        val permission = Manifest.permission.POST_NOTIFICATIONS
+//
+//        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+//            return
+//        }
+//
+//        requestPermissions(arrayOf(permission), 1)
+//    }
+//
+//    private fun checkGoogleApiAvailability() {
+//        with(GoogleApiAvailability.getInstance()) {
+//            val code = isGooglePlayServicesAvailable(this@AppActivity)
+//            if (code == ConnectionResult.SUCCESS) {
+//                return@with
+//            }
+//            if (isUserResolvableError(code)) {
+//                getErrorDialog(this@AppActivity, code, 9000)?.show()
+//                return
+//            }
+//            Toast.makeText(this@AppActivity, R.string.google_play_unavailable, Toast.LENGTH_LONG)
+//                .show()
+//        }
+//
+//        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+//            println(it)
+//        }
+//    }
+//}
